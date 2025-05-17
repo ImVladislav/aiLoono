b66fe032cbff3f88fd53cb331975bdf474df0c3c
Vladislav <nafanya102@gmail.com>
2025-05-17 21:59:54 +0300

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 const PORT = 4000;
 const TOKEN = process.env.TOKEN;
 
-const allowedOrigins = ['https://lmo-tawny.vercel.app', 'https://loomo.boo'];
+const allowedOrigins = ['https://loono.vercel.app', 'https://loono.boo'];
 
 app.use(
    cors({
